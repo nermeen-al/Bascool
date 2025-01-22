d4012d75d86b500707c7d4d638cc9247db6afd8d
nermeen16ali <nermeenali162000@gmail.com>
2025-01-22 15:57:15 +0200

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,25 @@ $(window).on("load", function () {
 		$("body").css({ "overflow-y": "visible" });
 	});
 });
+/* Theme
+=============================*/
+$('#theme-toggle').click(function () {
+	$(this).toggleClass('switch');
+
+});
+document.addEventListener('DOMContentLoaded', () => {
+	const themeToggleBtn = document.getElementById('theme-toggle');
+	const body = document.body;
+	themeToggleBtn.addEventListener('click', () => {
+		if (body.classList.contains('light')) {
+			body.classList.remove('light');
+			body.classList.add('dark');
+		} else {
+			body.classList.remove('dark');
+			body.classList.add('light');
+		}
+	});
+});
 
 document.getElementById('directionSwitch').addEventListener('click', function () {
 	var docDirection = document.documentElement.dir;
@@ -18,8 +37,46 @@ document.getElementById('directionSwitch').addEventListener('click', function ()
 ========================*/
 $(document).ready(function () {
 	"use strict";
+	$(".drop-link").click(function (e) {
+		$(".drop-list").slideUp(),
+			$(this).next().is(":visible") || $(this).next().slideDown(),
+			e.stopPropagation();
+	});
+	$(".drop-link-sec").click(function (e) {
+		$(".drop-sec").slideUp(),
+			$(this).next().is(":visible") || $(this).next().slideDown(),
+			e.stopPropagation();
+	});
+	$(".drop-link-thi").click(function (e) {
+		$(".drop-thi").slideUp(),
+			$(this).next().is(":visible") || $(this).next().slideDown(),
+			e.stopPropagation();
+	});
+	$(".drop-link-forth").click(function (e) {
+		$(".drop-fort").slideUp(),
+			$(this).next().is(":visible") || $(this).next().slideDown(),
+			e.stopPropagation();
+	});
+	$(".drop-link-fifth").click(function (e) {
+		$(".drop-fif").slideUp(),
+			$(this).next().is(":visible") || $(this).next().slideDown(),
+			e.stopPropagation();
+	});
+	$(".drop-link-sades").click(function (e) {
+		$(".drop-sadess").slideUp(),
+			$(this).next().is(":visible") || $(this).next().slideDown(),
+			e.stopPropagation();
+	});
+	
+	// Tooltip
+	$('[data-toggle="tooltip"]').tooltip();
+	//  Selec 2
 	if ($(".select").length > 0) {
 		$(".select").select2();
+	}
+	//  Tag
+	if ($(".tags").length > 0) {
+		$(".tags").select2();
 	}
 	// Toggle Button
 	$(".toggle-btn").click(function () {
@@ -27,4 +84,49 @@ $(document).ready(function () {
 		$(".dashboard_content").toggleClass('move');
 		$(".dash_header").toggleClass('move');
 	});
+	// Date Picker
+	if ($(".flatpickr-input").length > 0) {
+		$(".flatpickr-input").flatpickr({
+			enableTime: false,
+			dateFormat: "Y-m-d",
+		});
+	}
+	// Timer
+	if ($(".timer_count").length > 0) {
+		$(".timer_count").countTo();
+	}
+	// Data Table
+	if ($(".datatable").length > 0) {
+		$(".datatable").DataTable({
+			fixedHeader: true,
+			dom: "Bfrtip",
+			buttons: [
+				{
+					extend: "print",
+					text: '<i class="fa fa-print"></i> طباعة',
+					titleAttr: "print",
+				},
+				{
+					extend: "excelHtml5",
+					text: '<i class="far fa-file-excel"></i> أكسل',
+					titleAttr: "Excel",
+				},
+				{
+					extend: "pdfHtml5",
+					text: '<i class="far fa-file-pdf"></i> PDF',
+					titleAttr: "PDF",
+				},
+			],
+		});
+	}
+	$('#damg_div').hide(); // Initially hide the damg_div
+
+	$('select[name="student_type"]').change(function () {
+		if ($(this).val() === 'damg') {
+			$('#damg_div').show();
+		} else {
+			$('#damg_div').hide();
+		}
+	});
+	
 });
